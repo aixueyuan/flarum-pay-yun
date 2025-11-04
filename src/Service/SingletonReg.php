bb8148c4e95cb9bdc90362d3327c5f144169362d
aixueyuan <stuejiv88bhny23@gmail.com>
2025-11-05 01:04:47 +0800

    Update SingletonReg.php

--- src/Service/SingletonReg.php
+++ src/Service/SingletonReg.php
@@ -2,21 +2,10 @@
 
 namespace Neworder\PayYun\Service;
 
-use Illuminate\Contracts\Container\Container;
-
 class SingletonReg
 {
-    /**
-     * Register services into the Flarum IoC container.
-     *
-     * @param Container $container
-     */
-    public function register(Container $container)
+    public function register()
     {
-        // 在这里注册服务单例（目前留空即可）
-        // 示例：
-        // $container->singleton(MyService::class, function ($container) {
-        //     return new MyService();
-        // });
+        // 留空
     }
 }
