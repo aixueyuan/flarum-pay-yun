7c7ed1ef41f6c9314cf7dda461991a69f227bb39
aixueyuan <stuejiv88bhny23@gmail.com>
2025-11-05 00:54:47 +0800

    Add files via upload

---
+++ extend.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Neworder\PayYun;
+
+use Flarum\Extend;
+use Neworder\PayYun\Service\SingletonReg;
+
+return [
+    (new Extend\ServiceProvider())
+        ->register(SingletonReg::class)
+];
